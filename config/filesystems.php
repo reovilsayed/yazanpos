8b1139fdaa874c940102d1d08bfdf123ce89a496
KawshikBss <kawshikbss@gmail.com>
2024-09-24 16:31:39 +0600

    modified file paths

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,8 +38,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => public_path('uploads'),
-            'url' => env('APP_URL') . '/uploads',
+            'root' => public_path('storage'),
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
