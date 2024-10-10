00327b643a40fe7ddef7459912e67f5570057bd3
KawshikBss <kawshikbss@gmail.com>
2024-10-10 11:26:16 +0600

    added employee shift tracking

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\EmployeeShift;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
@@ -32,6 +34,15 @@ class LoginController extends Controller
             if ($request->hasSession()) {
                 $request->session()->put('auth.password_confirmed_at', time());
             }
+            if (auth()->user() && auth()->id()) {
+                $prevShift = EmployeeShift::where('user_id', auth()->id())->where('status', 1)->first();
+                if (!$prevShift) {
+                    EmployeeShift::create([
+                        'user_id' => auth()->id(),
+                        'clock_in' => now(),
+                    ]);
+                }
+            }
 
             return $this->sendLoginResponse($request);
         }
@@ -39,6 +50,27 @@ class LoginController extends Controller
         $this->incrementLoginAttempts($request);
 
         return $this->sendFailedLoginResponse($request);
+    }
+
+    public function logout(Request $request)
+    {
+        if (auth()->user() && auth()->id()) {
+            $prevShift = EmployeeShift::where('user_id', auth()->id())->where('status', 1)->first();
+            if ($prevShift) {
+                $prevShift->update([
+                    'clock_out' => now(),
+                    'status' => 2
+                ]);
+            }
+        }
+
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
     }
 
     protected function credentials(Request $request)
