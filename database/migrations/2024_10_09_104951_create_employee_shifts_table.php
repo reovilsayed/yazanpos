1a21d99b38e898c16a6db35b9e317edb4958f6a5
KawshikBss <kawshikbss@gmail.com>
2024-10-10 11:23:50 +0600

    created new tables and added columns

---
+++ database/migrations/2024_10_09_104951_create_employee_shifts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employee_shifts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->timestamp('clock_in')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('clock_out')->nullable();
+            $table->tinyInteger('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employee_shifts');
+    }
+};
