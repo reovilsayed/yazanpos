a5682ae7d2ca8147887290520648d5c5b0d25884
sayed khan <reovilsayed@gmail.com>
2024-10-20 08:30:32 +0600

    d

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,10 +13,10 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('send:due-reminder-emails')->monthly();
-        $schedule->command('reports:send-monthly-due')->monthlyOn(1, '12:00');
+        // $schedule->command('send:due-reminder-emails')->monthly();
+        // $schedule->command('reports:send-monthly-due')->monthlyOn(1, '12:00');
 
-        // $schedule->command('scrap:everymin')->everyMinute();
+         $schedule->command('close:inactive-user-shift')->everyMinute();
     }
 
     /**
